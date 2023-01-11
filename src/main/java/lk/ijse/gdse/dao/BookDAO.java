ecc042f021f90f6f5d104b5a91f5000d420d02a8
pradeep sampath <109191549+Pradeep-Sampath-2846@users.noreply.github.com>
2023-01-11 14:35:18 +0530

    implement update book

--- src/main/java/lk/ijse/gdse/dao/BookDAO.java
+++ src/main/java/lk/ijse/gdse/dao/BookDAO.java
@@ -1,17 +1,42 @@
 package lk.ijse.gdse.dao;
 
+import lk.ijse.gdse.db.DBConnection;
 import lk.ijse.gdse.entity.Book;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class BookDAO {
 
-    public Book saveBook(Book book){
-        return null;
+    private Connection connection;
+
+    public BookDAO() throws SQLException, ClassNotFoundException {
+
+        this.connection = DBConnection.getDbConnection().getConnection();
     }
 
-    public Book updateBook(Book book){
-        return null;
+    public Book saveBook(Book book) throws SQLException{
+        PreparedStatement stm = connection.prepareStatement("INSERT INTO Book (isbn, title, author,qty) VALUES (?,?,?,?)");
+        stm.setString(1,book.getIsbn());
+        stm.setString(2,book.getTitle());
+        stm.setString(3,book.getAuthor());
+        stm.setInt(4,book.getQty());
+        if (stm.executeUpdate()!=1) throw new RuntimeException("Failed to save the book");
+        return book;
+    }
+
+    public Book updateBook(Book book) throws SQLException {
+        PreparedStatement stm = connection.prepareStatement("UPDATE Book SET title=? ,author=? ,qty=? WHERE isbn=?");
+        stm.setString(1,book.getTitle());
+        stm.setString(2,book.getAuthor());
+        stm.setInt(3,book.getQty());
+        stm.setString(4,book.getIsbn());
+
+        if(stm.executeUpdate()!=1) throw new RuntimeException("Failed to update the book");
+        return book;
+
     }
 
     public void deleteByIsbn(String isbn){
