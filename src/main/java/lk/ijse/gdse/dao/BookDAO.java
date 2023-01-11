58d86e00761043eec8d3cb4bebf697f37cb54afe
pradeep sampath <109191549+Pradeep-Sampath-2846@users.noreply.github.com>
2023-01-11 15:00:34 +0530

    implement basic crud operations in BookDAO

--- src/main/java/lk/ijse/gdse/dao/BookDAO.java
+++ src/main/java/lk/ijse/gdse/dao/BookDAO.java
@@ -5,7 +5,9 @@ import lk.ijse.gdse.entity.Book;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BookDAO {
@@ -39,24 +41,50 @@ public class BookDAO {
 
     }
 
-    public void deleteByIsbn(String isbn){
-
+    public void deleteByIsbn(String isbn) throws SQLException {
+        PreparedStatement stm = connection.prepareStatement("DELETE FROM Book WHERE isbn=?");
+        stm.setString(1,isbn);
+        if(stm.executeUpdate()!=1) throw new RuntimeException("Failed to delete the book");
     }
 
-    public List<Book> findAll(){
-        return null;
+    public List<Book> findAll() throws SQLException {
+        PreparedStatement stm = connection.prepareStatement("SELECT * FROM Book");
+        ResultSet rst = stm.executeQuery();
+        List<Book> bookList= new ArrayList<>();
+        while (rst.next()){
+            Book book = new Book(rst.getString("isbn"), rst.getString("title"), rst.getString("author"), rst.getInt("qty"));
+            bookList.add(book);
+        }
+        return bookList;
     }
 
-    public long count(){
-        return 0;
+    public long count() throws SQLException {
+        PreparedStatement stm = connection.prepareStatement("SELECT COUNT(isbn) AS count FROM Book;");
+        ResultSet rst = stm.executeQuery();
+        rst.next();
+        return rst.getInt(1);
     }
 
-    public boolean existByIsbn(String isbn){
-        return true;
+    public boolean existByIsbn(String isbn) throws SQLException {
+        PreparedStatement stm = connection.prepareStatement("SELECT * FROM Book WHERE isbn=?");
+        ResultSet rst = stm.executeQuery();
+        return rst.next();
     }
 
-    public List<Book> searchByText(String text){
-        return null;
+    public List<Book> searchByText(String text) throws SQLException {
+        PreparedStatement stm = connection.prepareStatement("SELECT * FROM Book WHERE isbn LIKE ? OR title LIKE ? OR author LIKE ? ");
+        text="%"+text+"%";
+        for (int i = 0; i < 3; i++) {
+            stm.setString(i+1,text);
+        }
+        ResultSet rst = stm.executeQuery();
+        List<Book> bookList= new ArrayList<>();
+        while (rst.next()){
+            Book book = new Book(rst.getString("isbn"), rst.getString("title"), rst.getString("author"), rst.getInt("qty"));
+            bookList.add(book);
+        }
+        return bookList;
+
     }
 
 }
