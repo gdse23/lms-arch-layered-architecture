3c7a249fb671485dfb974caf6bbd41accba43a9b
pradeep sampath <109191549+Pradeep-Sampath-2846@users.noreply.github.com>
2023-01-16 12:10:50 +0530

    Implement IssueDAOImpl methods

--- src/main/java/lk/ijse/gdse/dao/custom/impl/IssueDAOImpl.java
+++ src/main/java/lk/ijse/gdse/dao/custom/impl/IssueDAOImpl.java
@@ -1,26 +1,51 @@
 package lk.ijse.gdse.dao.custom.impl;
 
 import lk.ijse.gdse.dao.custom.IssueDAO;
+import lk.ijse.gdse.dao.util.DBUtil;
+import lk.ijse.gdse.db.DBConnection;
 import lk.ijse.gdse.entity.Issue;
 
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public class IssueDAOImpl implements IssueDAO {
 
-    @Override
-    public Issue save(Issue entity) throws SQLException {
-        return null;
+    private final Connection connection = DBConnection.getDbConnection().getConnection();
+
+    public IssueDAOImpl() throws SQLException, ClassNotFoundException {
     }
 
     @Override
-    public Issue update(Issue entity) throws SQLException {
-        return null;
+    public Issue save(Issue issue) throws SQLException {
+        PreparedStatement stm = connection.prepareStatement
+                ("INSERT INTO issue SET  isbn=? ,memberId=?,date =?", Statement.RETURN_GENERATED_KEYS);
+        stm.setString(1,issue.getIsbn());
+        stm.setString(2,issue.getMemberId());
+        stm.setDate(3, Date.valueOf(LocalDate.now()));
+        stm.executeUpdate();
+        ResultSet generatedKeys = stm.getGeneratedKeys();
+        generatedKeys.next();
+        int issueId = generatedKeys.getInt(1);
+        issue.setIssueId(issueId);
+        return issue;
+
     }
 
     @Override
-    public void deleteByPk(Integer pk) throws SQLException {
+    public Issue update(Issue issue) throws SQLException, ClassNotFoundException {
+        if(DBUtil.executeUpdate("UPDATE  issue SET isbn =? , memberId=? WHERE issue_id =?",issue.getIsbn(),issue.getMemberId(),issue.getIssueId())){
+            return issue;
+        }
+        throw new RuntimeException("Failed to update the issue!");
+    }
+
+    @Override
+    public void deleteByPk(Integer issueId) throws SQLException, ClassNotFoundException {
+        if(!DBUtil.executeUpdate("DELETE FROM issue WHERE issue_id=?",issueId)){
+            throw new RuntimeException("Failed to delete the issue");
+        }
 
     }
 
@@ -30,17 +55,26 @@ public class IssueDAOImpl implements IssueDAO {
     }
 
     @Override
-    public Optional<Issue> findByPk(Integer pk) throws SQLException {
+    public Optional<Issue> findByPk(Integer issueId) throws SQLException, ClassNotFoundException {
+        ResultSet rst = DBUtil.executeQuery("SELECT * FROM issue WHERE issue_id =?", issueId);
+        if(rst.next()){
+            return Optional.of(new Issue(rst.getInt("issue_id"),rst.getString("isbn"),rst.getString("memberId"),rst.getDate("datet")));
+        }
+
         return Optional.empty();
+
     }
 
     @Override
-    public boolean existByPk(Integer pk) throws SQLException {
-        return false;
+    public boolean existByPk(Integer issueId) throws SQLException, ClassNotFoundException {
+        ResultSet rst = DBUtil.executeQuery("SELECT * FROM issue WHERE issue_id =?", issueId);
+        return rst.next();
     }
 
     @Override
-    public long count() throws SQLException {
-        return 0;
+    public long count() throws SQLException, ClassNotFoundException {
+        ResultSet rst = DBUtil.executeQuery("SELECT COUNT(issue_id) AS count FROM issue");
+        rst.next();
+        return rst.getInt(1);
     }
 }
