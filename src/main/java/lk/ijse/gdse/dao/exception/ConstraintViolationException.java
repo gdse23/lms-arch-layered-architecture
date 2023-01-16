984b6bab58983c92d2f9c507a63f085191370d64
pradeep sampath <109191549+Pradeep-Sampath-2846@users.noreply.github.com>
2023-01-16 16:32:35 +0530

    Add ConstraintViolationException

---
+++ src/main/java/lk/ijse/gdse/dao/exception/ConstraintViolationException.java
@@ -0,0 +1,23 @@
+package lk.ijse.gdse.dao.exception;
+
+public class ConstraintViolationException extends RuntimeException{
+
+    public ConstraintViolationException() {
+    }
+
+    public ConstraintViolationException(String message) {
+        super(message);
+    }
+
+    public ConstraintViolationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ConstraintViolationException(Throwable cause) {
+        super(cause);
+    }
+
+    public ConstraintViolationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
